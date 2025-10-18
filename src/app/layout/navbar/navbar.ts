4a4481cc1b12e01143cffa6463b6ff81d5ab027d
Bahico <ikromov0312@gmail.com>
2025-10-18 21:12:05 +0500

    starting project

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,18 +1,41 @@
 import {Component} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgClass, NgOptimizedImage} from '@angular/common';
 import {IconComponent} from '../../shared/components/icon/icon';
+import {TuiDropdownDirective, TuiDropdownManual, TuiDropdownOptionsDirective} from '@taiga-ui/core';
+import {TuiActiveZone, TuiObscured} from '@taiga-ui/cdk';
 
 @Component({
   selector: 'navbar',
   templateUrl: 'navbar.html',
+  styleUrl: 'navbar.css',
   imports: [
     NgOptimizedImage,
-    IconComponent
+    IconComponent,
+    TuiDropdownDirective,
+    TuiDropdownManual,
+    TuiObscured,
+    TuiActiveZone,
+    TuiDropdownOptionsDirective,
+    NgClass
   ],
   host: {
     class: 'flex w-full justify-center'
   }
 })
 export default class Navbar {
+  protected open = false;
 
+  protected onClick(): void {
+    this.open = !this.open;
+  }
+
+  protected onObscured(obscured: boolean): void {
+    if (obscured) {
+      this.open = false;
+    }
+  }
+
+  protected onActiveZone(active: boolean): void {
+    this.open = active && this.open;
+  }
 }
