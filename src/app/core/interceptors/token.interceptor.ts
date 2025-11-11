70d739c8e297e3382bc22a317277dba76fd3c026
Bahico <ikromov0312@gmail.com>
2025-11-11 20:11:38 +0500

    added basket-list

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,6 +1,6 @@
-import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import { inject } from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
+import {catchError, Observable, retry, throwError} from 'rxjs';
+import {inject} from '@angular/core';
 import {TokenStore} from '@/token';
 
 export default function (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
@@ -13,6 +13,16 @@ export default function (req: HttpRequest<unknown>, next: HttpHandlerFn): Observ
       },
     })
   }
-
-  return next(req);
+  console.log('aa')
+  return next(req)
+    .pipe(
+      retry(1),
+      catchError((err: HttpErrorResponse) => {
+        console.log(err)
+        if (err.status) {
+          tokenService.update = null;
+        }
+        return throwError(() => new Error(err.error.message || 'Something went wrong'));
+      })
+    );
 }
