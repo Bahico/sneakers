5e9a1a43d4f9b3919dbeec9c682a56929525b015
uniconsoft-macmini-1 <ikromov0312@gmail.com>
2025-11-21 18:19:41 +0500

    added reviews images
    Signed-off-by: uniconsoft-macmini-1 <ikromov0312@gmail.com>

--- src/app/pages/home/components/category/accessories/category-accessories.ts
+++ src/app/pages/home/components/category/accessories/category-accessories.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {CategoryDetail} from '@/home/components/category/detail/category-detail';
+import {Gender} from '@/home/home';
 
 @Component({
   templateUrl: 'category-accessories.html',
@@ -11,5 +12,6 @@ import {CategoryDetail} from '@/home/components/category/detail/category-detail'
   selector: 'category-accessories'
 })
 export class CategoryAccessories {
+  gender = input.required<Gender>();
 
 }
