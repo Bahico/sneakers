f45d55c96c3a2075314f928a78d7e32ac1be0a65
uniconsoft-macmini-1 <ikromov0312@gmail.com>
2025-12-24 10:54:14 +0500

    bugs fixed
    Signed-off-by: uniconsoft-macmini-1 <ikromov0312@gmail.com>

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client
   },
   {
+    path: 'product/:gender',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'home/:gender',
     renderMode: RenderMode.Client
   },
