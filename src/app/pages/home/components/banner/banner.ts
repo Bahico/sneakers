f6d4512ac9f0cffcc02228b511ac32d2302f5c54
bahico <ikromov0312@gmail.com>
2026-03-25 11:32:44 +0500

    changed reklama image

--- src/app/pages/home/components/banner/banner.ts
+++ src/app/pages/home/components/banner/banner.ts
@@ -2,11 +2,22 @@ import {BannerService} from '@/services/banner.service';
 import {Component, inject, signal} from "@angular/core";
 import {rxResource} from '@angular/core/rxjs-interop';
 import {map} from 'rxjs';
+import {GuaranteeOriginal} from '@/home/components/banner/guarantee-original/guarantee-original';
+import {MeetTeam} from '@/home/components/banner/meet-team/meet-team';
+import {BannerRealRate} from '@/home/components/banner/real-rate/banner-real-rate';
+import {BannerSplit} from '@/home/components/banner/split/banner-split';
+import {BannerTelegram} from '@/home/components/banner/telegram/banner-telegram';
 
 @Component({
   templateUrl: 'banner.html',
   selector: 'banner',
-  imports: [],
+  imports: [
+    GuaranteeOriginal,
+    MeetTeam,
+    BannerRealRate,
+    BannerSplit,
+    BannerTelegram
+  ],
   styles: [
     `
       .slider-wrapper {
@@ -17,15 +28,15 @@ import {map} from 'rxjs';
   ]
 })
 export class Banner {
-  private readonly bannerService = inject(BannerService);
+  // private readonly bannerService = inject(BannerService);
 
   activeIndex = signal(0);
 
-  protected readonly banners = rxResource({
-    stream: () => this.bannerService.banners().pipe(map(banners => {
-      banners.top_banners = banners.top_banners.sort((a, b) => a.order - b.order);
-      return banners;
-    })),
-  })
-  // slides = ['meet-team', 'split', 'guarantee-original', 'telegram', 'real-rate'] as const;
+  // protected readonly banners = rxResource({
+  //   stream: () => this.bannerService.banners().pipe(map(banners => {
+  //     banners.top_banners = banners.top_banners.sort((a, b) => a.order - b.order);
+  //     return banners;
+  //   })),
+  // })
+  slides = ['meet-team', 'split', 'guarantee-original', 'telegram', 'real-rate'] as const;
 }
