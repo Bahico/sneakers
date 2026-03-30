5e0177b16bc21e2f023364afd58d372598e13567
bahico <ikromov0312@gmail.com>
2026-03-30 11:48:19 +0500

    commit

--- src/app/pages/home/components/banner/guarantee-original/guarantee-original.ts
+++ src/app/pages/home/components/banner/guarantee-original/guarantee-original.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
+import {RouterLink} from '@angular/router';
+import {debounceTime, Subject} from 'rxjs';
 
 @Component({
   templateUrl: 'guarantee-original.html',
   selector: 'guarantee-original',
-  imports: []
+  imports: [
+    RouterLink
+  ]
 })
-export class GuaranteeOriginal {
-
-}
+export class GuaranteeOriginal {}
