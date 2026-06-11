c2b60620ddabbcbbfd6853e32bb862ef091fdb5f
bahico <ikromov0312@gmail.com>
2026-06-11 17:56:03 +0500

    changed texts

--- src/app/pages/product/detail/components/contacts/contacts-product.ts
+++ src/app/pages/product/detail/components/contacts/contacts-product.ts
@@ -7,13 +7,4 @@ import {ProductModel} from '@/models/product.model';
 })
 export class ContactsProduct {
   product = input<Partial<ProductModel>>();
-
-  isFootWear = computed(() => {
-    if (this.product()) {
-      if (this.product().category?.full_slug?.includes('footwear')) {
-        return true;
-      }
-    }
-    return false;
-  })
 }
